c41f0260f9f03553062bd1c5c4305cdf1d1e9c65
Nathan Graham Busse <82398683+Nathan-Busse@users.noreply.github.com>
2026-07-05 13:41:06 +0200

    Delete Core/SarcnetMiniSatelliteTrackerMK1/SourceCode/Code_Authers/Custom_ProjectSourceCode directory

--- Core/SarcnetMiniSatelliteTrackerMK1/SourceCode/Code_Authers/Custom_ProjectSourceCode/Nathan-Busse/CustomLibraries/GeoDudeLibrary/geodude/__init__.py
+++
@@ -1,5 +0,0 @@
-from .reverse_geocode import geodude
-from .data.install import create_db
-from .data.fetch import fetch_db
-
-__all__ = ['geodude', 'create_db', 'fetch_db']
--- Core/SarcnetMiniSatelliteTrackerMK1/SourceCode/Code_Authers/Custom_ProjectSourceCode/Nathan-Busse/DevTools/Tools/MagneticDeclinationCalculator/Debugging/gps_check.py
+++
@@ -1,8 +0,0 @@
-import serial
-import serial.tools.list_ports
-import pynmea2
-
-print("✅ Success! GPS libraries are installed and available.")
-print("📡 Available COM ports:")
-for port in serial.tools.list_ports.comports():
-    print(f" - {port.device}: {port.description}")
